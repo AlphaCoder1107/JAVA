d30a9d0b3f8a6fd27429275858ed185b39b97016
AlphaCoder <alphacoder1107@gmail.com>
2025-05-30 12:10:29 +0530

    NA

--- HELLO.java
+++ HELLO.java
@@ -9,5 +9,8 @@
 class HELLO {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.printf("Hello, %s!", "World");
+        
     }
 }
