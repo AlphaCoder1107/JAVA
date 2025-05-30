467f0ad19f011add378cc5544d1188d3afd04f3d
AlphaCoder <alphacoder1107@gmail.com>
2025-05-30 10:30:56 +0530

    Added test file

---
+++ TestClass.java
@@ -0,0 +1,9 @@
+/**
+ * Author   : AlphaCoder1107
+ * GitHub   : https://github.com/AlphaCoder1107
+ * Created  : 2025-05-30
+ * Purpose  : 
+ * License  : MIT License
+ */
+
+public class TestClass { public static void main(String[] args) {} }
